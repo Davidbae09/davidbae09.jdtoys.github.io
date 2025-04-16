4a064cf76d1a15110b838a9664502c0dc83c95dc
Davidbae09 <143783087+Davidbae09@users.noreply.github.com>
2025-04-16 12:57:09 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,6 +32,14 @@ function init() {
             handleEditProduct();
         }
     });
+
+    // Toggle hamburger menu
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+        hamburger.classList.toggle('active');
+    });
 }
 
 // Navigasi
